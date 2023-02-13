97f8af0e3c865b3254dabbcbda32edcb15d61da9
vzhiv <vzhiv@fvis.ru>
2023-02-13 16:02:25 +0300

    add method create string array

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Clear();
+
+string[] CreateStringArray()
+{
+    Console.Write("Input a number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[rows];
+    for(int i = 0; i < rows; i++)
+    {
+        Console.Write($"Input {i+1} row and press Enter: ");
+        array[i] = Console.ReadLine();
+        if(array[i] == string.Empty)
+        {
+            Console.WriteLine($"An empty string is entered, try again!");
+            i--;
+        }
+    }
+        
+    Console.WriteLine();
+    return array;
+}
+
+
+
+
+
