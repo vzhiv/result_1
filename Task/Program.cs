6148fffdb8d0fff332bd9a45968e3a0880437d32
vzhiv <vzhiv@fvis.ru>
2023-02-13 16:54:16 +0300

    add format code

--- Task/Program.cs
+++ Task/Program.cs
@@ -5,17 +5,17 @@ string[] CreateStringArray()
     Console.Write("Input a number of rows: ");
     int rows = Convert.ToInt32(Console.ReadLine());
     string[] array = new string[rows];
-    for(int i = 0; i < rows; i++)
+    for (int i = 0; i < rows; i++)
     {
-        Console.Write($"Input {i+1} row and press Enter: ");
+        Console.Write($"Input {i + 1} row and press Enter: ");
         array[i] = Console.ReadLine();
-        if(array[i] == string.Empty)
+        if (array[i] == string.Empty)
         {
             Console.WriteLine($"An empty string is entered, try again!");
             i--;
         }
     }
-        
+
     Console.WriteLine();
     return array;
 }
@@ -30,14 +30,14 @@ void ShowArray(string[] array)
 string[] LengthLess(string[] array)
 {
     int count = 0;
-    for(int i = 0; i < array.Length; i++) // ищем количество элементов в массиве, где длина строки меньше или равна 3-м символам. Делаем это для инициализации нового массива необходимого размера
-        if(array[i].Length <= 3) count++; 
-    
+    for (int i = 0; i < array.Length; i++) // ищем количество элементов в массиве, где длина строки меньше или равна 3-м символам. Делаем это для инициализации нового массива необходимого размера
+        if (array[i].Length <= 3) count++;
+
     string[] resultArray = new string[count];
-    for(int i = 0, j = 0; i < array.Length; i++, j++)
-        if(array[i].Length <= 3) resultArray[j] = array[i];
+    for (int i = 0, j = 0; i < array.Length; i++, j++)
+        if (array[i].Length <= 3) resultArray[j] = array[i];
         else j--;
-    
+
     return resultArray;
 }
 
